30d17ee68aed39cd78c9f8d0ab7a6dbe6948d019
Manish <manishtimalsina51@gmail.com>
2023-07-03 22:16:32 +0545

    added botton

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class ScoreApp extends StatelessWidget {
-  const ScoreApp ({super.key});
+  const ScoreApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -17,30 +17,100 @@ class ScoreApp extends StatelessWidget {
 }
 
 class ScoreHome extends StatefulWidget {
-  const ScoreHome({super.key});
+  const ScoreHome({Key? key}) : super(key: key);
 
   @override
   State<ScoreHome> createState() => _ScoreHomeState();
 }
 
 class _ScoreHomeState extends State<ScoreHome> {
+  int score = 0;
+
+  void increaseScore() {
+    setState(() {
+      score++;
+    });
+  }
+
+  void decreaseScore() {
+    setState(() {
+      score--;
+    });
+  }
+
+  void resetScore() {
+    setState(() {
+      score = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-        title:const Text("Score Changer"),
+      appBar: AppBar(
+        title: const Text("Score Changer"),
         centerTitle: true,
-
       ),
-
-      body: const Column(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.restore_outlined),
+        onPressed: resetScore,
+      ),
+      body: Column(
         children: [
-SizedBox(height:30,),
- Center(child: Text("Score is", style: TextStyle(color: Color.fromARGB(255, 159, 249, 13), fontSize: 30, fontWeight:FontWeight.bold),))
-
+          const SizedBox(
+            height: 30,
+          ),
+          const Center(
+            child: Text(
+              "Score is",
+              style: TextStyle(
+                color: Color.fromARGB(255, 200, 107, 31),
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              "$score",
+              style: TextStyle(
+                color: Color.fromARGB(255, 159, 249, 13),
+                fontSize: 80,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                  ),
+                  onPressed: increaseScore,
+                  child: const Text("Increase"),
+                ),
+                const SizedBox(
+                  width: 30,
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.red),
+                  ),
+                  onPressed: decreaseScore,
+                  child: const Text("Decrease"),
+                ),
+              ],
+            ),
+          )
         ],
-        ),
+      ),
     );
-  
   }
 }
